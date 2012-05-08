084cf36d9ff87d7283bac567ebc8436e32b87045
Mike Quinlan <mquinlan@mines.edu>
2012-05-08 10:10:36 -0600

    Adding scores scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Scoreboard::Application.routes.draw do
+  resources :scores, :only => [:index, :create]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
