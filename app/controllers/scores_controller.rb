dbb615f3fa306c1640e3a5cb879a72003b865b01
Mike Quinlan <mquinlan@mines.edu>
2012-05-09 22:35:05 -0600

    Cleaned up controller

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,4 @@
 class ScoresController < ApplicationController
-  before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
   
   # For all responses in this controller, return the CORS access control headers.
@@ -8,20 +7,6 @@ class ScoresController < ApplicationController
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
-  end
-  
-  # If this is a preflight OPTIONS request, then short-circuit the
-  # request, return only the necessary headers and return an empty
-  # text/plain.
-  
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
   end
   
   # GET /scores
