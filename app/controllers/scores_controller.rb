c20067726d56f17b90269d5e68c1eb6cda35dd65
Mike Quinlan <mquinlan@mines.edu>
2012-05-09 19:14:52 -0600

    changed scoreboard to return the first 3 results ordered by score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,7 +2,7 @@ class ScoresController < ApplicationController
   # GET /scores
   # GET /scores.json
   def index
-    @scores = Score.all
+    @scores = Score.order("score DESC").limit(3).select([:name, :score])
 
     respond_to do |format|
       #format.html # index.html.erb
